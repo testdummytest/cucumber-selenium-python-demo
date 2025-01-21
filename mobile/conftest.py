acb49ed1d529721fe6935c0310e3ba56d9cf59bb
yogesh <>
2025-01-21 18:06:00 +0530

    update

--- mobile/conftest.py
+++ mobile/conftest.py
@@ -1,19 +1,21 @@
 
 import pytest, logging, os
+from selenium import webdriver
+import chromedriver_autoinstaller
 from pytest_bdd import given, scenario, scenarios, when, then, parsers
 from time import sleep
 import random, json
 from selenium.webdriver.chrome.options import Options
 from contextlib import contextmanager
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium import webdriver
 
 
 @pytest.fixture()
 def driver(request):
     try:
-        c = Options()
+        
+        chromedriver_autoinstaller.install()
+        c = webdriver.ChromeOptions()   
+        # c = Options()
         c.add_argument("--headless=new")
         c.add_argument("--window-size=1920,1080")
         # c.add_argument("--no-sandbox")
@@ -22,9 +24,9 @@ def driver(request):
         # c.add_argument("--disable-dev-shm-usage")
         # prefs={"download.default_directory":os.getcwd()+"/downloads"}
         # c.add_experimental_option("prefs",prefs)
-        # driver = webdriver.Chrome(options=c)
+        driver = webdriver.Chrome(options=c)
         # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=c)
-        driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub',options=c)
+        # driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub',options=c)
         # driver = webdriver.Chrome()
         yield driver
     except Exception as e:
