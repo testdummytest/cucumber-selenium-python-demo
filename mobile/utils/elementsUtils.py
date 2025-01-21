259f1c83ef96f93ea421407c2b6f7e62494a8045
testdummytest <163297472+testdummytest@users.noreply.github.com>
2025-01-21 16:17:29 +0530

    Update elementsUtils.py

--- mobile/utils/elementsUtils.py
+++ mobile/utils/elementsUtils.py
@@ -2,7 +2,6 @@ import logging
 import time
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
-from appium.webdriver.common.appiumby import AppiumBy
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
