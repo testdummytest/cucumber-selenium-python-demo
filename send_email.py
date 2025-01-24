59232a8a9966f652321b311635592d79eb08e213
testdummytest <163297472+testdummytest@users.noreply.github.com>
2025-01-24 15:44:46 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -79,14 +79,14 @@ def send_mail(send_from, send_to, cc_list, subject, body_message, server, port, 
     msg['Subject'] = subject
     msg.attach(body_part)
     
-    shutil.make_archive("screenshots", 'zip', "screenshots")
+    # shutil.make_archive("screenshots", 'zip', "screenshots")
     # Attach report and log files
     with open("report/report.html", 'rb') as file:
         msg.attach(MIMEApplication(file.read(), Name="report.html"))
         
     # Attach screenshots.zip file
-    with open("screenshots.zip", 'rb') as file:
-        msg.attach(MIMEApplication(file.read(), Name="screenshots.zip"))
+    with open("screenshot.zip", 'rb') as file:
+        msg.attach(MIMEApplication(file.read(), Name="screenshot.zip"))
         
 
     msg["From"] = send_from
